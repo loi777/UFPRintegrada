f2fe188e114d74018e532bf3f8432a1a2d0e197a
Loi-Luiz Fernando <loi7777@hotmail.com>
2024-08-12 00:47:08 -0300

    Preenchido Ru e Intercampi v2

--- classes/Intercampi.java
+++ classes/Intercampi.java
@@ -5,4 +5,24 @@ public class Intercampi {
     public Intercampi() { }
 
     //---------------------------
+
+    public String[] getOnibus() {
+        return this.onibus;
+    }
+
+    public void setOnibus(String[] _onibus) {
+        this.onibus = _onibus;
+    }
+
+    //--
+
+    public String[][] getHorarios() {
+        return this.horarios;
+    }
+
+    public void setHorarios(String[][] _horarios) {
+        this.horarios = _horarios;
+    }
+
+    //---------------------------
 }
--- classes/RU.java
+++ classes/RU.java
@@ -1,9 +1,39 @@
 public class RU {
-    public String[] cafe;
-    public String[] almoco;
-    public String[] jantar;
+    private String[] cafe;
+    private String[] almoco;
+    private String[] jantar;
 
     public RU() { }
+
+    //---------------------------
+
+    public String[] getCafe() {
+        return this.cafe;
+    }
+
+    public void setCafe(String[] _cafe) {
+        this.cafe = _cafe;
+    }
+
+    //--
+
+    public String[] getAlmoco() {
+        return this.almoco;
+    }
+
+    public void setAlmoco(String[] _almoco) {
+        this.almoco = _almoco;
+    }
+
+    //--
+
+    public String[] getJantas() {
+        return this.jantar;
+    }
+
+    public void setJantar(String[] _jantar) {
+        this.jantar = _jantar;
+    }
 
     //---------------------------
 }
